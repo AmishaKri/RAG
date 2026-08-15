9ce7ca3f6fea1cf4de2d54f09f603b40737f3d60
samir kumar <samirkumar.aiml2021@nsec.ac.in>
2026-08-15 17:40:34 +0530

    feat: add FastAPI authentication

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,1 +1,26 @@
 # created by Copilot CLI runtime in VS Code - placeholder
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+from app.core.config import settings
+
+pwd_context = CryptContext(
+    schemes=['bcrypt'],
+    deprecated='auto'
+)
+
+def hash_pass(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_pass(plain_pass: str, hashed_pass: str) -> bool:
+    return pwd_context.verify(plain_pass, hashed_pass)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({"exp": expire})
+    
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+
